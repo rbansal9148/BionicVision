2a01599e90600af1c0fb97b98414a0dac0b2271c
Rubal Bansal <rbansal.rbansal@gmail.com>
2017-12-19 16:08:55 +0530

    Removed Buttons

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
 import scene_descriptor
 import picamera
 import boot
 import OCR
 
-#import button1
-#import button2
-#import button3
+# import button1
+# import button2
+# import button3
 
-#GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
 
-#pin1 = 26
-#pin2 = 23
-#pin3 = 16
+# pin1 = 26
+# pin2 = 23
+# pin3 = 16
 
-#GPIO.setup(pin1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(pin2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(pin3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.setup(pin1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.setup(pin2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.setup(pin3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Instatiate camera
 camera = picamera.PiCamera()
@@ -28,39 +28,39 @@ key = '431346910fd7435180c72f27b72c5f8d'
 scene_descriptor.bootstrap_MSC(key)
 
 
-try:
+# try:
 	# True for  getting button responses
-	while True:
-		print('Press a button')
-		helper.output_audio('Press a button...')
-		button_state1 = GPIO.input(pin1)
-		button_state2 = GPIO.input(pin2)
-		button_state3 = GPIO.input(pin3)
+	# while True:
+		# print('Press a button')
+		# helper.output_audio('Press a button...')
+		# button_state1 = GPIO.input(pin1)
+		# button_state2 = GPIO.input(pin2)
+		# button_state3 = GPIO.input(pin3)
 
-    	if button_state1 == False:
-		print('Capture And Detect...')
-		helper.output_audio('Capture And Detect...')
-		# key = scene_descriptor.getKey()
-        	try:
-        		scene_descriptor.camera_PR(key, camera)
-			scene_descriptor.camera_PR_DeepAI(_key_DeepAI, camera)
-    		except:
-        		print "Some Unknown Error"
-        		helper.output_audio("Some Unknown Error")
+    	# if button_state1 == False:
+print('Capture And Detect...')
+helper.output_audio('Capture And Detect...')
+# key = scene_descriptor.getKey()
+	try:
+		scene_descriptor.camera_PR(key, camera)
+	scene_descriptor.camera_PR_DeepAI(_key_DeepAI, camera)
+	except:
+		print "Some Unknown Error"
+		helper.output_audio("Some Unknown Error")
 
-		if button_state2 == False:
-			print('OCR Activated...')
-			helper.output_audio('OCR Activated...')
-			# time.sleep(0.2)
-			imageName = r'./abc.jpg'
-			camera.capture(imageName)
-			print OCR.get_string('./' + imageName)
-			helper.output_audio('./' + imageName)
+		# if button_state2 == False:
+print('OCR Activated...')
+helper.output_audio('OCR Activated...')
+# time.sleep(0.2)
+imageName = r'./abc.jpg'
+camera.capture(imageName)
+print OCR.get_string('./' + imageName)
+helper.output_audio('./' + imageName)
 
-		if button_state3 == False:
-			print('Sending Emergency Message...')
-			helper.output_audio('Sending Emergency Message...')
-			time.sleep(0.2)
+		# if button_state3 == False:
+print('Sending Emergency Message...')
+helper.output_audio('Sending Emergency Message...')
+time.sleep(0.2)
 
-except:
-    GPIO.cleanup()
+# except:
+#     GPIO.cleanup()
